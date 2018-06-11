df782d55967313db9f69d2bec1c0a87040247285
Cees de Groot <casedeg@gmail.com>
2018-06-11 08:57:10 -0400

    Use NanoVG GL2 for "regular" platforms, GLES2 for VideoCore

--- uderzo/c_src/uderzo_support.h
+++ uderzo/c_src/uderzo_support.h
@@ -24,7 +24,11 @@
 #endif
 
 #include <nanovg.h>
-#define NANOVG_GL2_IMPLEMENTATION
+#ifdef UDERZO_VC
+#  define NANOVG_GLES2_IMPLEMENTATION
+#else
+#  define NANOVG_GL2_IMPLEMENTATION
+#endif
 #include <nanovg_gl.h>
 #include <nanovg_gl_utils.h>
 
