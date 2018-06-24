99a4877047001acd958670a0d68c82ef413129b0
Cees de Groot <casedeg@gmail.com>
2018-06-24 13:16:14 -0400

    More cleanups

--- clixir/lib/clixir/code_writer.ex
+++ clixir/lib/clixir/code_writer.ex
@@ -3,17 +3,27 @@ defmodule Clixir.CodeWriter do
   This module does the actual work of `Clixir.__before_compile__/1`. It writes out
   the C and GNU gperf code fragments which later on get combined to form the project's
   `clixir` executable.
+
+  Due to the nature of the system, re-compilation is mostly unavoidable: if we are
+  called, then apparently the Elixir compiler decided that the Clixir-using module
+  was out of date. We could go all smart and drop a hash over the C code on disk and
+  check whether it was changed, but the executables we generate are small so recompilation
+  is not a big issue. Therefore, we set things up so that recompilations happens rather
+  often instead of trying to avoid it.
   """
 
   require Logger
 
-  # TODO only do this when needed (compare timestamps,etc)
-  #   (this is quite hairy so probably wants to dig into Elixir's dependency management)
   def write_code(module) do
     clixir_dir = Path.join(Mix.Project.build_path(), "clixir")
     :ok = File.mkdir_p(clixir_dir)
 
-    # Write C file
+    write_c_code(module, clixir_dir)
+    write_gperf_code(module, clixir_dir)
+
+  end
+
+  defp write_c_code(module, clixir_dir) do
     target = Path.join(clixir_dir, Atom.to_string(module))
     {:ok, target_file} = File.open(target <> ".c", [:write])
 
@@ -34,9 +44,13 @@ defmodule Clixir.CodeWriter do
       IO.puts(target_file, body)
     end)
     File.close(target_file)
+  end
 
-    # Dump data for gperf
+  defp write_gperf_code(module, clixir_dir) do
+    target = Path.join(clixir_dir, Atom.to_string(module))
     {:ok, target_file} = File.open(target <> ".gperf", [:write])
+
+    cfuns = Module.get_attribute(module, :cfuns)
     Enum.map(cfuns, fn {fun, _} -> IO.puts target_file, "#{fun}, _dispatch_#{fun}" end)
     File.close(target_file)
   end
