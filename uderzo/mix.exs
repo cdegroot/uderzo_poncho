7f9dab56f3d94644fd2481f9e9df969206ff9d8e
Cees de Groot <casedeg@gmail.com>
2018-06-25 22:24:25 -0400

    Remove RPi3 libraries from uderzo directory/package

--- uderzo/mix.exs
+++ uderzo/mix.exs
@@ -3,7 +3,7 @@ defmodule Uderzo.Mixfile do
 
   def project do
     [ app: :uderzo,
-      version: "0.5.2",
+      version: "0.5.3",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
@@ -67,7 +67,6 @@ defmodule Uderzo.Mixfile do
         "lib",
         "c_src",
         "priv/*.ttf",
-        "rpi_deps",
         "mix.exs",
         "Makefile",
         "setup.mk",
--- uderzo/rpi_deps/README.md
+++
@@ -1,6 +0,0 @@
-This directory contains the Erlang interface libraries and headers 
-from OTP 20.2 on the RPi3, and some other dependencies:
-* libfreetype.a, grabbed from the Raspbian libfreetype6-dev package
-* libpng.a, from the Raspbian libpng-dev package
-This could/should be a custom system at some point, but for now
-this is simpler to keep in sync.
