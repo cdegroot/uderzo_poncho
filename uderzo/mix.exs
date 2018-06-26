0bb80dac0f4cc90b650a1e72b5c7224d4d572269
Cees de Groot <casedeg@gmail.com>
2018-06-25 22:03:33 -0400

    Publish rpi_deps to hex.pm

--- uderzo/mix.exs
+++ uderzo/mix.exs
@@ -3,7 +3,7 @@ defmodule Uderzo.Mixfile do
 
   def project do
     [ app: :uderzo,
-      version: "0.5.1",
+      version: "0.5.2",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
@@ -67,6 +67,7 @@ defmodule Uderzo.Mixfile do
         "lib",
         "c_src",
         "priv/*.ttf",
+        "rpi_deps",
         "mix.exs",
         "Makefile",
         "setup.mk",
