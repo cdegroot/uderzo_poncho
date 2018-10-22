4e9b65bee6d528a63abcfe22a4b7aa6f4780e3a7
Cees de Groot <casedeg@gmail.com>
2018-10-22 19:15:05 -0400

    Bump version

--- uderzo/mix.exs
+++ uderzo/mix.exs
@@ -3,7 +3,7 @@ defmodule Uderzo.Mixfile do
 
   def project do
     [ app: :uderzo,
-      version: "0.7.0",
+      version: "0.8.0",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
