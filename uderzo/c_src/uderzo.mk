9423c9e23f8b8abace08346830e4010add89ea2d
Cees de Groot <casedeg@gmail.com>
2019-03-04 18:38:05 -0500

    MIX_TARGET appears to be always set on hex.publish

--- uderzo/c_src/uderzo.mk
+++ uderzo/c_src/uderzo.mk
@@ -17,7 +17,7 @@ ifeq ($(shell uname -m), armv7l)
 MIX_TARGET ?= rpi3
 endif
 
-ifdef MIX_TARGET
+ifeq ($(MIX_TARGET), rpi3)
 # We're cross-compiling under Nerves. Play Raspberry Pi.
 CFLAGS+=-DUDERZO_VC -DSTANDALONE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -D_LINUX -fPIC -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -Wall -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -ftree-vectorize -pipe -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -Wno-psabi
 LDFLAGS+=-L/opt/vc/lib/ -lbrcmGLESv2 -lbrcmEGL -lopenmaxil -lbcm_host -lvchostif -lvcos -lvchiq_arm -lpthread -lrt -lm
